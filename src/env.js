a06e9498a5427e0eedf331310c96b6973768d772
itsm16 <mrinalg2k1@gmail.com>
2026-08-12 22:48:16 +0530

    fix: folder structure

--- src/env.js
+++ src/env.js
@@ -12,9 +12,9 @@ export const env = createEnv({
     //     ? z.string()
     //     : z.string().optional(),
     // DATABASE_URL: z.string().url(),
-    // NODE_ENV: z
-    //   .enum(["development", "test", "production"])
-    //   .default("development"),
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
   },
 
   /**
@@ -33,7 +33,7 @@ export const env = createEnv({
   runtimeEnv: {
     // BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
     // DATABASE_URL: process.env.DATABASE_URL,
-    // NODE_ENV: process.env.NODE_ENV,
+    NODE_ENV: process.env.NODE_ENV,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
